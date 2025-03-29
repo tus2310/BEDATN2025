869b26ecf1d226042db2820848e7cb7bda448d76
Lynchlee <noslynchlee@gmail.com>
2025-03-29 21:38:51 +0700

    fix comment

--- src/comment.ts
+++ src/comment.ts
@@ -10,16 +10,6 @@ export interface Comment extends Document {
   rating?: number; // Đánh giá tùy chọn (1-5 sao)
 }
 
-const CommentSchema: Schema = new Schema({
-  id: { type: Number, unique: true },
-  user: { type: String, required: true },
-  text: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  productId: { type: String, required: true },
-  name: { type: String, required: true },
-  rating: { type: Number, min: 1, max: 5 },
-});
-
 const CommentModel = mongoose.model<Comment>("Comment", CommentSchema);
 
 export default CommentModel;
