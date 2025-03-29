fe3bbc67a4d05c33ca232751044ab15ede8e7291
ngocc <ngocnhaph39144@fpt.edu.vn>
2025-03-29 22:58:18 +0700

    Updtae lai biến thể

--- src/product.ts
+++ src/product.ts
@@ -33,43 +33,6 @@ const VariantSchema: Schema = new Schema(
   { timestamps: true }
 );
 
-const ProductSchema: Schema = new Schema(
-  {
-    masp: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    img: [{ type: String }],
-    moTa: { type: String, required: true },
-    brand: { type: String, required: true },
-    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-    status: { type: Boolean, required: true },
-    variants: [
-      {
-        size: { type: String, required: true },
-        color: { type: String, required: true },
-        quantity: { type: Number, required: true },
-        price: { type: Number, required: true },
-        discount: { type: Number, default: 0 },
-      },
-      { timestamps: true },
-    ],
-  },
-  { timestamps: true }
-);
-
-export function checkDuplicateVariants(variants: Variant[]): Error | null {
-  const variantSet = new Set();
-  for (const variant of variants) {
-    const key = `${variant.size}-${variant.color}`;
-    if (variantSet.has(key)) {
-      return new Error(
-        `Có biến thể trùng lặp trong sản phẩm: Size: ${variant.size}, Color: ${variant.color}`
-      );
-    }
-    variantSet.add(key);
-  }
-  return null;
-}
-
 ProductSchema.index({ masp: 1, name: 1 }, { unique: true });
 ProductSchema.plugin(mongoosePaginate);
 
